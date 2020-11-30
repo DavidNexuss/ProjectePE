fa8c5978d04dee8407311b1be6a68b020f58f6fb
hegocre <hegocre@hotmail.com>
2020-11-30 16:10:31 +0100

    Utilitza l'API amb autenticació

--- forksstars.bash
+++ forksstars.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -Ls "https://api.github.com/repos/$1/forks?sort=stargazers&per_page=100" | jq '.[] | "\(.name) \(.stargazers_count)"' | tr -d '"'
+curl -u $(tail -n 1 credentials.conf) -Ls "https://api.github.com/repos/$1/forks?sort=stargazers&per_page=100" | jq '.[] | "\(.name) \(.stargazers_count)"' | tr -d '"'
